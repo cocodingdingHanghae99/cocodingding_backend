78157b7fabe8046fb28591b71f343811c7d417cf
ggobsarikuna <hyunwu123@naver.com>
2023-02-07 16:26:13 +0900

    CI/CD restart

--- src/main/java/com/sparta/serviceteam4444/nginx/TestController.java
+++ src/main/java/com/sparta/serviceteam4444/nginx/TestController.java
@@ -1,13 +1,15 @@
 package com.sparta.serviceteam4444.nginx;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequiredArgsConstructor
 @RestController
 public class TestController {
     @GetMapping("/")
     public String success() {
-        return "성공 테스트 하기";
+
+        return "성공";
     }
 
 }
