0232278fde0bcd4ef3bbb779713fcbec73589a0b
kjk0122 <juke9548@gmail.com>
2023-02-08 21:55:00 +0900

    젭ㅂㅂㅂㅂ

--- src/main/java/com/sparta/serviceteam4444/controller/UserController.java
+++ src/main/java/com/sparta/serviceteam4444/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
         return userService.signup(signupRequestDto);
     }
     @ApiOperation(value = "카카오 로그인", notes = "이것은 카카오 로그인 버튼을 누름을 통해서 수행된다.")
-    @RequestMapping(value = "/kakao", method = RequestMethod.GET)    //카카오로부터 코드 받고, 다시 전달해서
+    @RequestMapping(value = "/login/kakao", method = RequestMethod.GET)    //카카오로부터 코드 받고, 다시 전달해서
     public ResponseDto kakaoLogin(@RequestParam(value="code") String code, HttpServletResponse response) throws JsonProcessingException {
        String createToken = kakaoService.kakaoLogin(code);
 
