b0492bed8dd26b4c15d753f3b964004f8976fbcc
kjk0122 <juke9548@gmail.com>
2023-02-09 19:02:41 +0900

    다시 프론트 측 uri로 redirect uri 변경

--- src/main/java/com/sparta/serviceteam4444/service/KakaoService.java
+++ src/main/java/com/sparta/serviceteam4444/service/KakaoService.java
@@ -56,7 +56,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "dca78b23ee6bbb566b637457b88b9de0");
-        body.add("redirect_uri", "http://cocodingding.shop/user/kakao");
+        body.add("redirect_uri", "http://localhost:3000/user/kakao");
         body.add("code", code);
 
         // HTTP 요청 보내기
