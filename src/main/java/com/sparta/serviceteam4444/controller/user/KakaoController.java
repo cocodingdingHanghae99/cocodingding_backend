1210a2c2b8f16fd4b65788cdbbc2d790a9800750
ggobsarikuna <hyunwu123@naver.com>
2023-02-17 23:48:31 +0900

    requestparam ("code") 추가

--- src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
+++ src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
@@ -16,7 +16,7 @@ public class KakaoController {
     private KakaoService kakaoService;
 
     @GetMapping("/kakao")
-    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public String kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) throws JsonProcessingException {
 
         String createToken = kakaoService.kakaoLogin(code, response);
 
