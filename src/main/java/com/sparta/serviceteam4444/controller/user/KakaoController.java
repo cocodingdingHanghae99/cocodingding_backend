71b9af6d69147e03eaf5f566400c11d8b7cf5067
ggobsarikuna <hyunwu123@naver.com>
2023-02-17 13:22:35 +0900

    서버 재시작

--- src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
+++ src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
@@ -1,9 +1,15 @@
 package com.sparta.serviceteam4444.controller.user;
 
+import com.sparta.serviceteam4444.dto.user.kakao.KakaoLoginRequestDto;
+import com.sparta.serviceteam4444.dto.user.kakao.KakaoLoginResponseDto;
 import com.sparta.serviceteam4444.service.user.KakaoService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Api(tags = {"Kakao"})
 @RestController
@@ -14,5 +20,11 @@ public class KakaoController {
 
     private final KakaoService kakaoService;
 
+    @ApiOperation(value = "인가코드 받아오기")
+    @PostMapping("/kakao")
+    public ResponseEntity<KakaoLoginResponseDto> kakaoLogin(@RequestBody KakaoLoginRequestDto kakaoLoginRequestDto){
+//        return ResponseEntity.created(URI.create("/kakao")).body(kakaoService.kakaoLogin(kakaoLoginRequestDto));
+        return null;
+    }
 
 }
---
+++ src/main/java/com/sparta/serviceteam4444/dto/user/kakao/KakaoLoginRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.serviceteam4444.dto.user.kakao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KakaoLoginRequestDto {
+}
