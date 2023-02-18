7991c47384ad1a0a2ea60c241b00112a975a4ae2
ggobsarikuna <hyunwu123@naver.com>
2023-02-18 13:04:38 +0900

    test

--- src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
+++ src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
@@ -17,8 +17,9 @@ public class KakaoController {
 
     private KakaoService kakaoService;
 
-    @PostMapping("/kakao")
-    public String kakaoLogin(@RequestParam(value = "code") String code, HttpServletResponse response) throws JsonProcessingException {
+    @GetMapping("/kakao")
+    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+
         //requestbody도 생각해볼 것
 
         return kakaoService.kakaoLogin(code, response);
