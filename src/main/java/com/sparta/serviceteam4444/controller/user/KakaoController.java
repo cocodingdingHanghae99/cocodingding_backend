6a141b7cb8fbcca64a1ff9a7d31d368a0d59e89e
ggobsarikuna <hyunwu123@naver.com>
2023-02-18 10:27:56 +0900

    Post요청, requestbody로 바꿈

--- src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
+++ src/main/java/com/sparta/serviceteam4444/controller/user/KakaoController.java
@@ -3,11 +3,13 @@ package com.sparta.serviceteam4444.controller.user;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sparta.serviceteam4444.jwt.JwtUtil;
 import com.sparta.serviceteam4444.service.user.KakaoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/user")
 @CrossOrigin("http://localhost:3000")
@@ -15,8 +17,8 @@ public class KakaoController {
 
     private KakaoService kakaoService;
 
-    @GetMapping("/kakao")
-    public String kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    @PostMapping("/kakao")
+    public String kakaoLogin(@RequestBody String code, HttpServletResponse response) throws JsonProcessingException {
         //requestbody도 생각해볼 것
 
         return kakaoService.kakaoLogin(code, response);
