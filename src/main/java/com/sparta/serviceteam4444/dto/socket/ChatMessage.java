0e4525b4ac9d5e50da1b1f596ac3c6ad17743e37
ggobsarikuna <hyunwu123@naver.com>
2023-02-23 14:42:11 +0900

    enum

--- src/main/java/com/sparta/serviceteam4444/dto/socket/ChatMessage.java
+++ src/main/java/com/sparta/serviceteam4444/dto/socket/ChatMessage.java
@@ -10,11 +10,11 @@ import lombok.Setter;
 public class ChatMessage {
 
     // 메시지 타입 : 입장, 채팅
-    public enum MessageType {
-            ENTER, TALK
-    }
-
-    private MessageType type; // 메시지 타입
+//    public enum MessageType {
+//            ENTER, TALK
+//    }
+//
+//    private MessageType type; // 메시지 타입
 
     private String roomId; // 방번호
 
