b27d0acb4bce65be34bae00103c9e5235353a3da
ggobsarikuna <hyunwu123@naver.com>
2023-02-27 15:48:42 +0900

    test

---
+++ src/test/java/com/sparta/serviceteam4444/service/user/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.sparta.serviceteam4444.service.user;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    UserService userService;
+
+    @Test
+    void testReferenceType(){
+
+    }
+  
+}
