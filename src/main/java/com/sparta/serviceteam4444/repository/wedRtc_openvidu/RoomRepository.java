230827c0c73bd94e49c0ded824d288ecee63e95b
kingscode99 <118798407+kingscode99@users.noreply.github.com>
2023-03-03 16:34:02 +0900

    방 역순으로 보여주기

--- src/main/java/com/sparta/serviceteam4444/repository/wedRtc_openvidu/RoomRepository.java
+++ src/main/java/com/sparta/serviceteam4444/repository/wedRtc_openvidu/RoomRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
     Page<Room> findByOrderByModifiedAtDesc(PageRequest pageable);
+    //Page<Room> findByOrderByModifiedAt(PageRequest pageable);
 }
